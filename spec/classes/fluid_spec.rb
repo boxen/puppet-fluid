d2cc565660d5a9e566ded7b791ad64131f2e824a
Graham Gilbert <graham@grahamgilbert.com>
2014-02-26 14:00:49 +0000

    Completely re-worked tests

--- spec/classes/fluid_spec.rb
+++ spec/classes/fluid_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 describe 'fluid' do
-
-  it { should contain_class('fluid') }
-  it { should contain_package('Fluid-1.7.2').with_provider('compressed_app') }
-  it { should contain_package('Fluid-1.7.2').with_source("http://www.fluidapp.com/dist/Fluid_1.7.2.zip") }
-
+    it do
+      should contain_package('Fluid-1.7.2').with({
+        :provider => 'compressed_app',
+        :source   => 'http://www.fluidapp.com/dist/Fluid_1.7.2.zip',
+      })
+    end
 end
